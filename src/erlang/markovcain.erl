2820d55238e07a0d2403e54eb85316dc353bfa2e
ibrown <ibrown@twitter.com>
2016-06-11 15:25:48 -0700

    dict generation mostly working

--- src/erlang/markovcain.erl
+++ src/erlang/markovcain.erl
@@ -6,9 +6,10 @@ markovcain() ->
 markovcain(Dict) ->
     case io:get_chars('', 65536) of
 	eof ->
+	    print_dict(Dict),
 	    init:stop();
 	Text ->    
-	    io:put_chars(Text),
+	    %% io:put_chars(Text),
 	    NewDict = parse(Text, Dict),
 	    markovcain(NewDict)
     end.
@@ -22,6 +23,8 @@ markovcain(Dict) ->
 parse(Text, Dict)->
     Words = string:tokens(Text, " .!,"),
     count(Words, Dict).
+%%    print_dict(Result).
+
 
 count([H|T], Dict)->
     case T of
@@ -32,22 +35,37 @@ count([], Dict) ->
     Dict.
 
 increment(Root, Node, Dict)->
-    case dict:is_key(Root, Dict) of
-	true ->
-	    Counts = dict:fetch(Root, Dict),
-	    case lists:keyfind(Node, 1, Counts) of 
+   % io:fwrite("root: ~p node: ~p~n", [Root, Node]),
+   %     io:fwrite("dict: ~p~n~n", [Dict]),
+    case Node of
+	[] -> Dict;
+	_ ->
+
+	    case dict:is_key(Root, Dict) of  
+		true ->
+		    Counts = dict:fetch(Root, Dict),
+		    case lists:keyfind(Node, 1, Counts) of 
+			false ->
+			    UpdatedCount = lists:flatten([Counts, [{Node, 1}]]);
+			{Node, Count} ->
+			    UpdatedCount =  lists:keyreplace(Node, 1, Counts, {Node, Count + 1})
+		    end,
+		    dict:store(Root, UpdatedCount, Dict);
 		false ->
-		    UpdatedCount = lists:flatten([Counts, [{Node, 1}]]);
-		{Node, Count} ->
-		    UpdatedCount =  lists:keyreplace(Node, 1, Counts, {Node, Count + 1})
-	    end,
-	    dict:store(Root, UpdatedCount, Dict);
-	false ->
-	    dict:store(Root, [{Node, 1}], Dict)
+		    dict:store(Root, [{Node, 1}], Dict)
+	    end
     end.
+
 		    
 		     		    
-		    
+print_dict(Dict)->
+
+    F = fun(Key, Value, AccIn) ->
+		io:fwrite("~p~n", [Key]),
+		[io:fwrite("   ~p ~p~n", [Word, Count]) || {Word, Count} <- Value],
+		AccIn
+	end,
+    dict:fold(F, [], Dict).
 	    
 
 
