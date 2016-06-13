e9fcb692dcbec6f3962cad21b66f3c55de67ad94
ibrown <ibrown@twitter.com>
2016-06-12 21:55:43 -0700

    still buggy, but getting chains

--- src/erlang/markovcain.erl
+++ src/erlang/markovcain.erl
@@ -2,12 +2,23 @@
 -export([markovcain/0, parse/2]).
 
 markovcain() ->
-  markovcain(dict:new()).
+    random:seed(now()),
+    markovcain(dict:new()).
 markovcain(Dict) ->
     case io:get_chars('', 65536) of
 	eof ->
-	    print_dict(Dict),
-	    io:format("~p~n", [generate_probability_tables(Dict)]),
+	    %print_dict(Dict),
+	    Tables = generate_probability_tables(Dict),
+	 %   io:format("~p~n", [Tables]),
+	    Keys = dict:fetch_keys(Dict),
+	    FirstWord = lists:nth(random:uniform(length(Keys)), Keys),
+	    NextWord = next_word(FirstWord, Tables),
+	    NextWord2 = next_word(NextWord, Tables),
+	    NextWord3 = next_word(NextWord2, Tables),
+
+	    io:format("~s ~s ~s ~s~n", [FirstWord, NextWord, NextWord2, NextWord3]),
+	    
+	    
 	    init:stop();
 	Text ->    
 	    %% io:put_chars(Text),
@@ -100,8 +111,21 @@ add_range([], _,Acc) ->
     
     
     
-%next_word(Word, Dict)->	    
-%    lists 
+next_word(Word, ProbabilityTables)->	    
+    io:format("word is ~p~n", [Word]),
+    {Word, NextWords} = lists:keyfind(Word, 1, ProbabilityTables), 
+    Random = random:uniform(100), 
+    [Result | _]= lists:filter(fun(Entry) ->
+					   {range_floor, Floor} = lists:keyfind(range_floor, 1, Entry),
+					   {range_ceiling, Ciel} = lists:keyfind(range_ceiling, 1, Entry),
+					io:format("~p < ~p < ~p ~n", [Floor, Random, Ciel]),   
+				       (Floor =< Random) and (Random =< Ciel)
+				   end, NextWords),
+    {word, NextWord} = lists:keyfind(word, 1, Result),
+
+ 
+    NextWord.
+
 
 
 	     
